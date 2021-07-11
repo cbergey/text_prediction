af3d60e4dd9f988a915819541cd5d44e2a0826b6
Lauren Oey <loey@ucsd.edu>
2021-07-10 19:58:36 -0700

    stimuli presentation and text box, and react to new inputted words
    Former-commit-id: 1f58d07532fbf1f73d58a19bbf3f49163e261d6a

--- text-prediction-game/js/vars.js
+++ text-prediction-game/js/vars.js
@@ -2,15 +2,29 @@ var expt = { //add conditions here
     saveURL: 'submit.simple.php',
     startPage: 'trial', // {'consent','trial'}
     totalTrials: 2, //adjust to how many trials you have
-    debug: false //set to false when ready to run
+    nStimTrial: 3,
+    debug: false, //set to false when ready to run
+    stimOrder: []
 };
 
 var trial = {
     number: 1, //which trial is this? //1-indexed
+    stim: [],
     startTime: 0,
     endTime: 0,
     totalTime: 0
 }
+
+var word = {
+    number: 0,
+    prediction: '',
+    text: '',
+    startTime: 0,
+    time: 0,
+    cumulTime: 0
+
+}
+
 var client = parseClient();
 var trialData = []; //store all data in json format
 var vidData = [];
