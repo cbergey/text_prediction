cf82c4a99f34f0fcce369845771c464f20cef5fd
Lauren Oey <loey@ucsd.edu>
2021-07-10 19:58:36 -0700

    stimuli presentation and text box, and react to new inputted words
    Former-commit-id: 7269bb5a157b3edc8020ce7edf1c81ad2e232d1f

--- text-prediction-game/js/stim.js
+++ text-prediction-game/js/stim.js
@@ -1,1 +1,14 @@
-var stim = []
+var stim = [
+	"luca.jpg",
+	"nemo.jpg",
+	"wall-e.jpg",
+	"up.jpg",
+	"bugs-life.jpg",
+	"soul.jpg"
+];
+
+var ngram = {
+	"0" : "adult_childes_unigram.json",
+	"1" : "adult_childes_bigram.json",
+	"2" : "adult_childes_trigram.json"
+}
