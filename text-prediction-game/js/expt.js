7269bb5a157b3edc8020ce7edf1c81ad2e232d1f
Lauren Oey <loey@ucsd.edu>
2021-07-10 19:58:36 -0700

    stimuli presentation and text box, and react to new inputted words

--- text-prediction-game/js/expt.js
+++ text-prediction-game/js/expt.js
@@ -1,6 +1,7 @@
 
 
 function pageLoad(){
+    expt.stimOrder = shuffle(stim);
     clicksMap[expt.startPage]();
 }
 
@@ -26,14 +27,56 @@ function trialStart(){
     //$('#next').attr('disabled',true);
     $('#round').html('Round ' + trial.number + " of " + expt.totalTrials);
     trial.startTime = new Date().getTime(); //reset start of trial time
+    word.startTime = new Date().getTime(); //reset word timer
     //function here of experiment
-    
+    $('#stimRow').html(presentStim(expt.nStimTrial));
+    listenResponse();
 }
 
-function trialDone(){
-    $('#trial').css('display','none');
-    trial.endTime = new Date().getTime();
-    trial.totalTime = trial.endTime - trial.startTime;
+function presentStim(numStim){
+    var stimTable = "";
+    trial.stim = [];
+    for(i=0; i<numStim; i++) {
+        var thisStim = expt.stimOrder[(trial.number-1)*numStim+i];
+        trial.stim.push(thisStim);
+        stimTable += "<div class='stimCell'><img class='stimImg' src='img/stim-img/"+thisStim+"'></div>";
+    }
+    return(stimTable);
+}
+
+
+function getPredFile(){ // get ngram files value from dict
+    var nWords = getStringLen($('#response').val());
+    nWords = Math.max(nWords, 2); // if # words > 2, reduce to 2
+    return(ngram[nWords.toString()]); // convert to string and get ngram value
+}
+
+function getPrediction(){
+    var nDict = getPredFile();
+    //stuff here to get prediction from dictionary
+}
+
+function listenResponse(){
+    var keyPress = function(e){
+        var key = e.keyCode;
+        if(key == 32){
+            console.log("word done!");
+            word.time = new Date().getTime() - word.startTime;
+            saveData(); //save data after every word
+            word.startTime = new Date().getTime(); //reset word timer
+        }
+    }
+    $("#response").keydown(keyPress);
+}
+
+function clearListenResponse(){
+    $('#response').keydown(null);
+    $('#response').val('');
+}
+
+function saveData(){
+    var fullResponse = $('#response').val();
+    word.text = getLastNWords(fullResponse, 1);
 
     recordData();
     // these lines write to server
@@ -41,7 +84,17 @@ function trialDone(){
     if(!expt.debug){
         writeServer(data);
     }
+}
 
+
+function trialDone(){
+    $('#trial').css('display','none');
+    
+    trial.endTime = new Date().getTime();
+    trial.totalTime = trial.endTime - trial.startTime;
+
+    saveData();
+    clearListenResponse();
     
     // if we are done with all trials, then go to completed page
     if(trial.number == expt.totalTrials){
@@ -51,6 +104,7 @@ function trialDone(){
     else {
         // increase trial number
         ++trial.number;
+        trial.endTime = null;
         trialStart();
     }
 }
@@ -59,9 +113,14 @@ function recordData(){
     // record what the subject did in json format
     trialData.push({
         trialNumber: trial.number,
-        trialTime: trial.totalTime,
+        trialStim: trial.stim,
         startTime: trial.startTime,
-        endTime: trial.endTime
+        wordNumber: word.number,
+        textPrediction: word.prediction,
+        textResponse: word.text,
+        wordTime: word.time,
+        trialCumulTime: word.cumulTime
+        
     });
 }
 
@@ -79,36 +138,9 @@ function experimentDone(){
 
 
 
-    ////////////////////////////
-   //////// Microphone ////////
-  ////////////////////////////
-
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-var audiotRecorder = null;
-
-function turnOnMicrophone(){
-    var constraints = {audio:true};
-    var preview = document.querySelector('.videostream');
-    var recording = document.querySelector('#recording');
-    navigator.mediaDevices.getUserMedia(constraints).
-    then(stream => {
-        //put something here
-    })
-}
-
-function turnOffMicrophone(){
-
-}
-
-// function saveAudio(){
-//     audioRecorder.exportWAV(doneEncoding);
-
-//     function doneEncoding( blob ) {
-//         Recorder.setupDownload( blob, "myRecording" + ((recIndex<10)?"0":"") + recIndex + ".wav" );
-//         recIndex++;
-//     }
-
-// }
+    //////////////////////////////////
+   //////// Helper Functions ////////
+  //////////////////////////////////
 
 
 function shuffle(array){ //shuffle list of objects
@@ -125,4 +157,26 @@ function debugLog(message) {
     if(expt.debug){
         console.log(message);
     }
+}
+
+function getSplitStr(string){
+    return(string.split(" "));
+}
+
+function getStringLen(string) {
+    if(string == ''){
+        return(0);
+    } else{
+        return(getSplitStr(string).length);
+    }
+}
+
+function getLastNWords(string, n) {
+    var l = getStringLen(string);
+    var ws = getSplitStr(string);
+    var wArr = [];
+    for(var i=l-n; i<l; i++){
+        wArr.push(ws[i]);
+    }
+    return(wArr.join(' '));
 }
