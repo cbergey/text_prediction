8e8bcde8fcfe0232363365c789e455a54c12021b
Lauren Oey <loey@ucsd.edu>
2021-07-20 10:58:06 -0700

    array of dictionaries of posttest questions
    Former-commit-id: 2deab1d64202f0d830ff5e95aa5d44c26b8c3e4e

---
+++ text-prediction-game/js/posttestQs.js
@@ -0,0 +1,13 @@
+var postQs = [
+	{
+		"id": "trust",
+		"question": "How much do you trust text prediction?",
+		"type": "slider",
+		"answers": ["not at all", "a lot"]
+	}, {
+		"id": "predictOn",
+		"question": "Do you keep prediction settings on?",
+		"type": "radio",
+		"answers": ["yes", "no"]
+	}
+]
