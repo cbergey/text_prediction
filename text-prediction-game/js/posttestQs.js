f20cb9f3437994b470767644ce72ffa90197b8f6
Lauren Oey <loey@ucsd.edu>
2021-07-20 14:54:31 -0700

    updated posttest questions

--- text-prediction-game/js/posttestQs.js
+++ text-prediction-game/js/posttestQs.js
@@ -1,13 +1,43 @@
 var postQs = [
 	{
+		"id": "predictOn",
+		"question": "Do you usually keep prediction settings on?",
+		"type": "radio",
+		"answers": ["yes", "no"]
+	}, {
 		"id": "trust",
 		"question": "How much do you trust text prediction?",
 		"type": "slider",
 		"answers": ["not at all", "a lot"]
 	}, {
-		"id": "predictOn",
-		"question": "Do you usually keep prediction settings on?",
-		"type": "radio",
-		"answers": ["yes", "no"]
+		"id": "influenceYou",
+		"question": "How much do you think predictive text suggestions influence your writing?",
+		"type": "slider",
+		"answers": ["not at all", "a lot"]
+	}, {
+		"id": "influenceOthers",
+		"question": "How much do you think predictive text suggestions influence people's writing in general?",
+		"type": "slider",
+		"answers": ["not at all", "a lot"]
+	}, {
+		"id": "storyGeneric",
+		"question": "How generic do you think your interpretation of the story was?",
+		"type": "slider",
+		"answers": ["not at all", "very generic"]
+	}, {
+		"id": "storyCreative",
+		"question": "How creative do you think your interpretation of the story was?",
+		"type": "slider",
+		"answers": ["not at all", "very creative"]
+	}, {
+		"id": "stress",
+		"question": "How stressed did this task make your feel?",
+		"type": "slider",
+		"answers": ["not at all", "very stressed"]
+	}, {
+		"id": "smooth",
+		"question": "How smooth was your experience of doing this task?",
+		"type": "slider",
+		"answers": ["not at all", "very smooth"]
 	}
 ]
