a9c7320073886932acfaf3cbe43147c33585d1aa
shoshana-simons <87144895+shoshana-simons@users.noreply.github.com>
2021-07-21 19:57:32 -0400

    demographic & post-test change
    updated demographic questions (corresponding json update still needed), reorganized order of post-test questions & added question

--- text-prediction-game/js/posttestQs.js
+++ text-prediction-game/js/posttestQs.js
@@ -1,24 +1,9 @@
 var postQs = [
 	{
-		"id": "predictOn",
-		"question": "Do you usually keep prediction settings on?",
-		"type": "radio",
-		"answers": ["yes", "no"]
-	}, {
-		"id": "trust",
-		"question": "How much do you trust text prediction?",
+		"id": "smooth",
+		"question": "How smooth was your experience of doing this task?",
 		"type": "slider",
-		"answers": ["not at all", "a lot"]
-	}, {
-		"id": "influenceYou",
-		"question": "How much do you think predictive text suggestions influence your writing?",
-		"type": "slider",
-		"answers": ["not at all", "a lot"]
-	}, {
-		"id": "influenceOthers",
-		"question": "How much do you think predictive text suggestions influence people's writing in general?",
-		"type": "slider",
-		"answers": ["not at all", "a lot"]
+		"answers": ["not at all", "very smooth"]
 	}, {
 		"id": "storyGeneric",
 		"question": "How generic do you think your interpretation of the story was?",
@@ -34,10 +19,30 @@ var postQs = [
 		"question": "How stressed did this task make your feel?",
 		"type": "slider",
 		"answers": ["not at all", "very stressed"]
-	}, {
-		"id": "smooth",
-		"question": "How smooth was your experience of doing this task?",
+	},{
+		"id": "correct",
+		"question": "The sequences of images given to you were drawn from a storybook. How close do you think your telling of the story was to the original?",
 		"type": "slider",
-		"answers": ["not at all", "very smooth"]
+		"answers": ["not at all close", "very close"]
+	}, {
+		"id": "predictOn",
+		"question": "In general, do you keep text prediction settings on?",
+		"type": "radio",
+		"answers": ["yes", "no," "sometimes"]
+	}, {
+		"id": "trust",
+		"question": "In general, how much do you trust text prediction?",
+		"type": "slider",
+		"answers": ["not at all", "a lot"]
+	}, {
+		"id": "influenceYou",
+		"question": "In general, how much do you think predictive text suggestions influence your writing?",
+		"type": "slider",
+		"answers": ["not at all", "a lot"]
+	}, {
+		"id": "influenceOthers",
+		"question": "In general, how much do you think predictive text suggestions influence people's writing?",
+		"type": "slider",
+		"answers": ["not at all", "a lot"]
 	}
 ]
