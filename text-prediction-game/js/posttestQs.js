27d2ee3cea4e2063e50d20b4888bfc092967bad5
shoshana-simons <87144895+shoshana-simons@users.noreply.github.com>
2021-07-22 13:18:49 -0400

    Update posttestQs.js
    changed wording of generic question; modified order of questions

--- text-prediction-game/js/posttestQs.js
+++ text-prediction-game/js/posttestQs.js
@@ -6,9 +6,14 @@ var postQs = [
 		"answers": ["not at all", "very smooth"]
 	}, {
 		"id": "storyGeneric",
-		"question": "How generic do you think your interpretation of the story was?",
+		"question": "How similiar do you think your telling of the story was to other participants' interpretations?",
 		"type": "slider",
-		"answers": ["not at all", "very generic"]
+		"answers": ["not at all", "very similiar"]
+	}, {
+		"id": "correct",
+		"question": "The sequences of images given to you were drawn from a storybook. How close do you think your telling of the story was to the original?",
+		"type": "slider",
+		"answers": ["not at all close", "very close"]
 	}, {
 		"id": "storyCreative",
 		"question": "How creative do you think your interpretation of the story was?",
@@ -19,11 +24,6 @@ var postQs = [
 		"question": "How stressed did this task make your feel?",
 		"type": "slider",
 		"answers": ["not at all", "very stressed"]
-	},{
-		"id": "correct",
-		"question": "The sequences of images given to you were drawn from a storybook. How close do you think your telling of the story was to the original?",
-		"type": "slider",
-		"answers": ["not at all close", "very close"]
 	}, {
 		"id": "predictOn",
 		"question": "In general, do you keep text prediction settings on?",
